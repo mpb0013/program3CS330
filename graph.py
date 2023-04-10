eeb8e372c09b9f686bf8781fa7d2f7eee289a680
mpb0013 <100956697+mpb0013@users.noreply.github.com>
2023-04-10 15:52:01 -0500

    graph

--- graph.py
+++ graph.py
@@ -10,6 +10,17 @@ class Node:
         self.z = z
         self.connections = []
 
+    def printNode(self):
+        print(
+        self.node_num,
+        self.status,  # unvisited = 1, open = 2, closed = 3
+        self.cost_so_far,
+        self.estimated_heuristic,
+        self.estimated_total,
+        self.previous_node,
+        self.x,
+        self.z)
+
 class Connection:
     def __init__(self, conn_num, from_node, to_node, cost):
         self.conn_num = conn_num
@@ -17,20 +28,54 @@ class Connection:
         self.to_node = to_node
         self.cost = cost
 
+    def printConnection(self):
+        print(
+        self.conn_num,
+        self.from_node,
+        self.to_node,
+        self.cost)
+
 class Graph:
     def __init__(self):
         self.nodes = []
         self.connections = []
+
+    def buildGraph(self, nodeFile, conFile):
+        with open(nodeFile, 'r') as fp:
+            for line in fp:
+                currentLine = line.strip()
+                if not currentLine.startswith('#'):
+                    info = line.split(',')
+                    self.add_node(info[1], info[7], info[8]) 
+                   #print(info[1], info[7], info[8])
+
+        with open(conFile, 'r') as fp:
+            for line in fp:
+                currentLine = line.strip()
+                if not currentLine.startswith('#'):
+                    info = line.split(',')
+                    self.add_connection(info[1], int(info[2]), int(info[3]), int(info[4])) 
+                    #print(info[1], int(info[2]), int(info[3]), int(info[4]))
+                    
+    def printGraph(self):
+        for n in self.nodes:
+            n.printNode()
+        
+        for c in self.connections:
+            c.printConnection()
 
     def add_node(self, node_num, x, z):
         node = Node(node_num, x, z)
         self.nodes.append(node)
 
     def add_connection(self, conn_num, from_node_num, to_node_num, cost):
-        from_node = self.nodes[from_node_num]
-        to_node = self.nodes[to_node_num]
+        from_node = self.nodes[from_node_num - 1]
+        to_node = self.nodes[to_node_num - 1]
         connection = Connection(conn_num, from_node, to_node, cost)
         from_node.connections.append(connection)
         to_node.connections.append(connection)
         self.connections.append(connection)
 
+g = Graph()
+g.buildGraph('CS 330, Pathfinding, Graph AB Nodes v3.txt', 'CS 330, Pathfinding, Graph AB Connections v3.txt')
+g.printGraph()
